6720ebe1ef65c2a93c2fc1b981732835a5e4f495
domi <491431567@qq.com>
2021-02-14 19:37:34 +0800

    init

---
+++ src/main/java/xdx/note/framework/XdxNoteFrameworkApp.java
@@ -0,0 +1,12 @@
+package xdx.note.framework;
+
+import org.springframework.boot.SpringApplication;
+
+public class XdxNoteFrameworkApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XdxNoteFrameworkApp.class, args);
+    }
+
+
+}
