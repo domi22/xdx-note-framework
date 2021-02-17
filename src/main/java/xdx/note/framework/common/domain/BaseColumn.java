1a87927f9c98a4598cc212e714d3c1c5bb5a8169
domi <491431567@qq.com>
2021-02-17 12:12:32 +0800

    db column

---
+++ src/main/java/xdx/note/framework/common/domain/BaseColumn.java
@@ -0,0 +1,62 @@
+package xdx.note.framework.common.domain;
+
+import java.util.Date;
+
+public class BaseColumn extends Id {
+
+    private String createBy;
+    private String createName;
+    private Date createTime;
+
+    private String updateBy;
+    private String updateName;
+    private Date updateTime;
+
+    public String getCreateBy() {
+        return createBy;
+    }
+
+    public void setCreateBy(String createBy) {
+        this.createBy = createBy;
+    }
+
+    public String getCreateName() {
+        return createName;
+    }
+
+    public void setCreateName(String createName) {
+        this.createName = createName;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getUpdateBy() {
+        return updateBy;
+    }
+
+    public void setUpdateBy(String updateBy) {
+        this.updateBy = updateBy;
+    }
+
+    public String getUpdateName() {
+        return updateName;
+    }
+
+    public void setUpdateName(String updateName) {
+        this.updateName = updateName;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+}
---
+++ src/main/java/xdx/note/framework/common/domain/Id.java
@@ -0,0 +1,15 @@
+package xdx.note.framework.common.domain;
+
+public class Id {
+
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+}
